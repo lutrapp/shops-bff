d29bbe020b62da087a4932d80ef7eb5bfc05439f
lutrapp <lutrapp@gmail.com>
2024-12-04 21:29:16 -0300

    initial commit

---
+++ src/test/java/com/lu_shop/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lu_shop.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
